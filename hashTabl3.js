4e4f8af507006fe99265ff0203e35ea9a6436160
Najil Parakkal <najilparakkal@gmail.com>
2024-01-31 16:43:27 +0530

    queue

---
+++ hashTabl3.js
@@ -0,0 +1,62 @@
+class hashTable{
+    constructor(size){
+        this.table = new Array(size)
+        this.size = size
+    }
+    hash(key){
+        let total = 0
+        for(let i=0 ; i<key.length;i++){
+            total += key.charCodeAt(i)
+        }
+        return total % this.size
+    }
+    set (key,value){
+        const index = this.hash(key)
+        const bucket = this.table[index]
+        if(!bucket){
+            this.table[index] = [[key,value]]
+        }else{
+            const sameKey = bucket.find(item=>item[0] === value)
+            if(sameKey){
+                sameKey[1] = value
+            }else{
+                bucket.push([key,value])
+            }
+        }
+    }
+    get(key){
+        const index = this.hash(key)
+         const bucket = this.table[index]
+         if(bucket){
+            const sameKey = bucket.find(item=> item[0] === key)
+            if(sameKey){
+                return sameKey[1]
+            }
+         }
+         return undefined
+    }
+    remove(key){
+        const index = this.hash(key)
+        const bucket = this.table[index]
+        if(bucket){
+            const sameKey  = bucket.find(item=>item[0] === key)
+            if(sameKey){
+                bucket.splice(bucket.indexOf(sameKey),1)
+            }
+        }
+    }
+    print(){
+        for(let i=0;i<this.table.length;i++){
+            if(this.table[i]){
+                console.log(i,this.table[i]);
+            }
+        }
+    }
+}
+const table = new hashTable(20)
+table.set("name" ,"NAJIL")
+table.set("age",19)
+table.set("name" ,"akhil")
+table.remove("name")
+table.print()
+console.log(table.get("name"));
