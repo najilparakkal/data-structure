b305a7db1e991c22ab53d5aa7ef33fb65add2a5d
Najil Parakkal <najilparakkal@gmail.com>
2024-02-06 22:00:09 +0530

    week 2

--- week 2.js
+++ week 2.js
@@ -1,203 +1,125 @@
-// class hashTable{
-//     constructor(size){
-//         this.table = new Array(size)
-//         this.size = size
-//     }
-//     hash(key){
-//         let total = 0
-//         for(let i=0;i<key.length;i++){
-//             total += key.charCodeAt(i)
-//         }
-//         return total %this.size
-//     }
-//     set(key,value){
-//         const index  = this.hash(key)
-//         const bucket = this.table[index]
-//         if(!bucket){
-//             this.table[index] = [[key,value]]
-//         }else{
-//             const sameKey = bucket.find(item=>item [0] === value)
-//             if(sameKey){
-//                 sameKey[1] = [value]
-//             }else{
-//                 bucket.push([key,value])
-//             }
-//         }
-//     }
-//     get(key){
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if(bucket){
-//             const sameKey = bucket.find(item => item [0] === key)
-//             if(sameKey){
-//                 return sameKey[1]
-//             }
-//         }
-//         return undefined
-//     }
-//     remove(key){
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if(bucket){
-//             const sameKey = bucket.find(item => item [0] === key)
-//             if(sameKey){
-//                 bucket.splice(bucket.indexOf(sameKey),1)
-//             }
-//         }
-//     }
-//     reverse(key){
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if(bucket){
-//             const sameKey = bucket.find(item => item [0] === key)
-//             if(sameKey){
-//                 return  String(sameKey[1]).split("").reverse().join("") 
-//             }
-//         }
-//     }
-//     print(){
-//         for(let i=0;i<this.table.length;i++){
-//             if(this.table[i]){
-//                 console.log(i,this.table[i]);
-//             }
-//         }
-//     }
-// }
-// const table = new hashTable(10)
-// table.set("name","najil")
-// table.set("week",14)
-// table.set("place" , "malappuram")
-
-// table.print()
-// console.log(table.reverse("week"));
-
-
-// function quickSort(arr,left=0,right = arr.length-1){
-//     if(left<right){
-//         let pivot = partition(arr,left,right)
-//         quickSort(arr,left,pivot-1)
-//         quickSort(arr,pivot+1,right)
-//     }
-//     return arr
-// }
-// function partition(arr,left,right){
-//     let pivot = arr[right]
-//     let i= left-1
-//     for(let j= left;j<right;j++){
-//         if(arr[j]<pivot){
-//             i++;
-//             swap(arr,i,j)
-//         }
-//     }
-//     swap(arr,i+1,right)
-//     return i+1
-// }
-// function swap(arr,i,j){
-//     let temp = arr[i]
-//     arr[i] = arr[j]
-//     arr[j] = temp
-// }
-// let arr = [6,5,4,3,2,1]
-// console.log(quickSort(arr));
-
-
-
-
-// function insertionSort(arr){
-//     for(let i=1;i<arr.length;i++){
-//         let insertionNumber = arr[i]
-//         let j= i-1
-//         while(j>=0 && arr[j] > insertionNumber){
-//             arr[j+1] = arr[j]
-//             j--
-//         }
-//         arr[j+1] = insertionNumber
-//     }
-
-// }
-// let arr = [6,5,4,3,2,1]
-// insertionSort(arr)
-// console.log(arr);
-
-
-// function selectionSort(arr){
-//     for(let i=0;i<arr.length-1;i++){
-//         let min = i
-//         for(let j=i+1;j<arr.length;j++){
-//             if(arr[j]<arr[min]){
-//                 min = j
-//             }
-//         }
-//         if(min != i){
-//             let temp = arr[i]
-//             arr[i] = arr[min]
-//             arr[min] = temp
-//         }
-//     }
-// }
-// let arr= [6,5,4,3,2,1]
-// selectionSort(arr)
-// console.log(arr);
-
-
-
-// class stack{
-//     constructor(){
+// class stack {
+//     constructor() {
 //         this.element = []
 //     }
-//     push(data){
+//     push(data) {
 //         this.element.push(data)
 //     }
-//     pop(){
+//     pop() {
 //         return this.element.pop()
 //     }
-//     peek(){
-//         return this.element[this.element.length-1]
-//     }
-//     largest(){
-//         let largest = this.element[0]
-//         for(let i=1;i<this.element.length;i++){
-//             if(largest<this.element[i]){
-//                 largest = this.element[i]
-//             }
-//         }
-//         return largest
-//     }
-//     dups(){
-//         let uniqarr = []
-//         for(let i=0;i<this.element.length-1;i++){
-//             for(let j=i+1;j<this.element.length;j++){
-//                 if(this.element[i] === this.element[j]){
-//                     uniqarr.push(this.element[i])
+
+//     validParanthisis(str) {
+//         let stack = []
+//         for (let char of str) {
+//             if (char === "(" || char === "[" || char === "{") {
+//                 stack.push(char)
+//             } else {
+//                 let oposite = stack.pop()
+//                 if ((char === "}" && oposite !== "{") || (char === "]" && oposite !== "[") || (char === ")" && oposite !== "(") || oposite === undefined) {
+//                     return false
 //                 }
 //             }
 //         }
-//         return uniqarr
-        
+//         return stack.length === 0
 //     }
-//     removeDup(){
-//         for(let i=0;i<this.element.length-1;i++){
-//             for(let j=i+1 ;j<this.element.length;j++){
-//                 if(this.element[i] === this.element[j]){
-//                     this.element.splice(j, 1);
-//                     j--;
+//     getDups() {
+//         let dups = []
+//         for (let i = 0; i < this.element.length - 1; i++) {
+//             for (let j = i + 1; j < this.element.length; j++) {
+//                 if (this.element[i] === this.element[j]) {
+//                     dups.push(this.element[i])
+//                 }
+//             }
+//         }
+//         return dups
+//     }
+//     removeDups() {
+//         for (let i = 0; i < this.element.length - 1; i++) {
+//             for (let j = i + 1; j < this.element.length; j++) {
+//                 if (this.element[i] === this.element[j]) {
+//                     this.element.splice(j, 1)
+//                     j--
 //                 }
 //             }
 //         }
 //     }
-//     print(){
+//     peek() {
+//         return this.element[this.element.length - 1]
+//     }
+//     print() {
 //         console.log(this.element);
 //     }
 // }
+
 // const st = new stack()
 // st.push(10)
-// st.push(20)
-// st.push(30)
-// st.push(20)
 // st.push(10)
+// st.push(120)
+// st.push(40)
+// st.push(30)
+
 // st.print()
-// console.log(st.dups());
-// st.removeDup()
+// st.removeDups()
 // st.print()
 
-
+class hashTable{
+    constructor(size){
+        this.table = new Array(size)
+        this.size = size
+    }
+    hash(key){
+        let total = 0
+        for(let i=0;i<key.length;i++){
+            total += key.charCodeAt(i)
+        }
+        return total %this.size
+    }
+    set(key,value){
+        const index = this.hash(key)
+        const bucket = this.table[index]
+        if(!bucket){
+            this.table[index] = [[key,value]]
+        }else{
+            const samkey = bucket.find(item=>item[0]=== value)
+            if(samkey){
+                samkey[1] = [value]
+            }else{
+                bucket.push([key,value])
+            }
+        }
+    }
+    get(key){
+        const index = this.hash(key)
+        const bucket = this.table[index]
+        if(bucket){
+            const samkey = bucket.find(item=>item[0] === key)
+            if(samkey){
+                return samkey[1]
+            }
+        }
+        return undefined
+    }
+    reverse(key){
+        const index = this.hash(key)
+        const bucket = this.table[index]
+        if(bucket){
+            const samkey = bucket.find(item=>item[0] === key)
+            if(samkey){
+                return String(samkey[1]).split("").reverse().join("")
+            }
+        }
+    }
+    print(){
+        for(let i=0;i<this.table.length;i++){
+            if(this.table[i]){
+                console.log(i,this.table[i]);
+            }
+        }
+    }
+}
+const table = new hashTable(10)
+table.set("name","sahal")
+table.set("week",15)
+table.print()
+console.log(table.reverse("name"));
