59f09d36f52de988ee9f384f13a86aeded04f2f1
Najil Parakkal <najilparakkal@gmail.com>
2024-02-09 13:55:14 +0530

    week 2

--- week 2.js
+++ week 2.js
@@ -1,76 +1,8 @@
-// function quickSort(arr, left = 0, right = arr.length - 1) {
-//     if (left < right) {
-//         let pivot = partiotion(arr, left, right)
-//         quickSort(arr, left, pivot - 1)
-//         quickSort(arr, pivot + 1, right)
-//     }
-//     return arr
-// }
-// function partiotion(arr, left, right) {
-//     let pivot = arr[right]
-//     let i = left - 1
-//     for (let j = left; j < right; j++) {
-//         if (arr[j] < pivot) {
-//             i++
-//             swap(arr, i, j)
-//         }
-//     }
-//     swap(arr, i + 1, right)
-//     return i + 1
-// }
-// function swap(arr, i, j) {
-//     let temp = arr[i]
-//     arr[i] = arr[j]
-//     arr[j] = temp
-// }
-// let arr = [6, 5, 4, 3, 2, 1]
-// console.log(quickSort(arr));
-
-
-
-// function mergeSort(arr) {
-//     if (arr.length < 2) {
-//         return arr
-//     }
-//     let mid = Math.floor(arr.length / 2)
-//     let leftArr = arr.slice(0, mid)
-//     let right = arr.slice(mid)
-//     return merge(mergeSort(leftArr), mergeSort(right))
-// }
-// function merge(leftArr, rightArr) {
-//     let sortArr = []
-//     while (leftArr.length && rightArr.length) {
-//         if (leftArr[0] <= rightArr[0]) {
-//             sortArr.push(leftArr.shift())
-//         } else {
-//             sortArr.push(rightArr.shift())
-//         }
-//     }
-//     return [...sortArr, ...leftArr, ...rightArr]
-// }
-// let arr = [6, 5, 4, 3, 2, 1]
-// console.log(mergeSort(arr));
-
-// function insertionSort(arr) {
-//     for (let i = 1; i < arr.length; i++) {
-//         let insertionNumber = arr[i]
-//         let j = i - 1
-//         while (j >= 0 && arr[j] > insertionNumber) {
-//             arr[j + 1] = arr[j]
-//             j--
-//         }
-//         arr[j + 1] = insertionNumber
-//     }
-// }
-// let arr = [6, 5, 4, 3, 2, 1]
-// insertionSort(arr)
-// console.log(arr);
-
 // function bubbleSort(arr) {
 //     let swapped;
 //     do {
 //         swapped = false
-//         for (let i = 0; i < arr.length; i++) {
+//         for (let i = 0; i < arr.length - 1; i++) {
 //             if (arr[i] > arr[i + 1]) {
 //                 let temp = arr[i]
 //                 arr[i] = arr[i + 1]
@@ -80,91 +12,268 @@
 //         }
 //     } while (swapped)
 // }
-// let arr = [6, 5, 4, 3, 23, 2]
+// let arr = [5, 3, 2, 1]
 // bubbleSort(arr)
 // console.log(arr);
 
+// function insertionSort(arr){
+//     for(let i=1;i<arr.length;i++){
+//         let insertionNumber = arr[i]
+//         let j= i-1;
+//         while(j>=0 && arr[j]>insertionNumber){
+//             arr[j+1] = arr[j]
+//             j--
+//         }
+//         arr[j+1] = insertionNumber
 
-// class hashTable {
-//     constructor(size) {
-//         this.table = new Array(size)
-//         this.size = size
 //     }
-//     hash(key) {
-//         let total = 0
-//         for (let i = 0; i < key.length; i++) {
-//             total += key.charCodeAt(i)
-//         }
-//         return total % this.size
-//     }
-//     set(key, value) {
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if (!bucket) {
-//             this.table[index] = [[key, value]]
-//         } else {
-//             const sameKey = bucket.find(item => item[0] === value)
-//             if (sameKey) {
-//                 sameKey[1] = [value]
-//             } else {
-//                 bucket.push([key, value])
+
+// }
+// let arr=[6,5,4,3,2,1]
+// insertionSort(arr)
+// console.log(arr);
+
+
+// function selectionSort(arr){
+//     for(let i=0;i<arr.length-1;i++){
+//         let min = i
+//         for(let j=i+1;j<arr.length;j++){
+//             if(arr[j]<arr[min]){
+//                 min = j
 //             }
 //         }
-//     }
-//     get(key) {
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if (bucket) {
-//             const sameKey = bucket.find(item => item[0] === key)
-//             if (sameKey) {
-//                 return sameKey[1]
-//             }
-//         }
-//         return undefined
-//     }
-//     reverse(key) {
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if (bucket) {
-//             const sameKey = bucket.find(item => item[0] === key)
-//             if (sameKey) {
-//                 return String(sameKey[1]).split("").reverse().join("")
-//             }
-//         }
-//         return undefined
-//     }
-//     remove(key) {
-//         const index = this.hash(key)
-//         const bucket = this.table[index]
-//         if (bucket) {
-//             const sameKey = bucket.find(item => item[0] === key)
-//             if (sameKey) {
-//                 return bucket.splice(bucket.indexOf(sameKey[1]), 1)
-//             }
-//         }
-//     }
-//     print() {
-//         for (let i = 0; i < this.table.length; i++) {
-//             if (this.table[i]) {
-//                 console.log(i, this.table[i]);
-//             }
+//         if(min != i){
+//             let temp = arr[i]
+//             arr[i] = arr[min]
+//             arr[min] = temp
 //         }
 //     }
 // }
-// const table = new hashTable(10)
-// table.set("name", "najil")
-// table.set("week", 14)
-// table.set("week", 15)
-// table.print()
-// console.log(table.reverse("week"));
-// table.print()
+// let arr=[6,5,4,3,2,1]
+// selectionSort(arr)
+// console.log(arr);
+
+// function mergeSort(arr){
+//     if(arr.length <2){
+//        return  arr
+//     }
+//     let mid = Math.floor(arr.length/2)
+//     let leftArr = arr.slice(0,mid)
+//     let rightArr = arr.slice(mid)
+//     return merge(mergeSort(leftArr),mergeSort(rightArr))
+// }
+// function merge(leftArr,rightArr){
+//     let sortArr = []
+//     while(leftArr.length && rightArr.length){
+//         if(leftArr[0]<= rightArr[0]){
+//             sortArr.push(leftArr.shift())
+//         }else{
+//             sortArr.push(rightArr.shift())
+//         }
+//     }
+//     return [...sortArr,...leftArr,...rightArr]
+// }
+// let arr= [54,32,52,2,6]
+// console.log(mergeSort(arr));
 
 
 
+
+// function quickSort(arr,left=0,right=arr.length-1){
+//     if(left<right){
+//         let pivot = partition(arr,left,right)
+//         quickSort(arr,left,pivot-1)
+//         quickSort(arr,pivot+1,right)
+//     }
+//     return arr
+// }
+// function partition(arr,left,right){
+//     let pivot = arr[right]
+//     let i = left-1
+//     for(let j=left;j<right;j++){
+//         if(arr[j]<pivot){
+//             i++
+//             swap(arr,i,j)
+//         }
+//     }
+//     swap (arr,i+1,right)
+//     return i+1
+// }
+// function swap(arr,i,j){
+//     let temp = arr[i]
+//     arr[i] = arr[j]
+//     arr[j] = temp
+// }
+// let arr = [6,5,4,3,2,1]
+// console.log(quickSort(arr));
+
+
+
+    // class hashTable{
+    //     constructor(size){
+    //         this.table = new Array(size)
+    //         this.size = size
+    //     }
+    //     hash(key){
+    //         let total = 0;
+    //         for(let i=0;i<key.length;i++){
+    //             total += key.charCodeAt(i)
+    //         }
+    //         return total %this.size
+    //     }
+    //     set(key,value){
+    //         const index = this.hash(key)
+    //         const bucket = this.table[index]
+    //         if(!bucket){
+    //             this.table[index] = [[key,value]]
+    //         }else{
+    //             const samkey = bucket.find(item => item[0] === value)
+    //             if(samkey){
+    //                 samkey[1] = [value]
+    //             }else{
+    //                 bucket.push([key,value])
+    //             }
+    //         }
+    //     }
+    //     get(key) {
+    //         const index = this.hash(key);
+    //         const bucket = this.table[index];
+    //         let items = []
+    //         if(bucket){
+    //            for(let item of bucket){
+    //             items.push(item[1])
+    //            }
+    //         }
+    //         return items.length >0 ? items:undefined
+    //     }
+    //     reverse(key){
+    //         const index = this.hash(key)
+    //         const bucket = this.table[index]
+    //         if(bucket){
+    //             const samkey = bucket.find(item => item [0] === key)
+    //             if(samkey){
+    //                 return samkey[1]=String(samkey[1]).split("").reverse().join("")
+    //             }
+    //         }
+    //     }
+    //     remove(key){
+    //         const index = this.hash(key)
+    //         const bucket = this.table[index]
+    //         if(bucket){
+    //             const samkey = bucket.find(item => item [0] === key)
+    //             if(samkey){
+    //                 return bucket.splice(bucket.indexOf(samkey),1)
+    //             }
+    //         }
+    //     }
+    //     print(){
+    //         for(let i=0;i<this.table.length;i++){
+    //             if(this.table[i] ){
+    //                 console.log(i,this.table[i]);
+    //             }
+    //         }
+    //     }
+    // }
+    // const table = new hashTable(10)
+    // table.set("name","najil")
+    // table.set("week",14)
+    // table.set("BCE",133)
+    // table.set("BCE",144)
+    
+    // table.print()
+    // console.log(table.get("BCE"));
+    // console.log(table.reverse("name"));
+    
+
+
+
+
+    // class queue{
+//     constructor(){
+//         this.items = []
+//         this.rear = 0;
+//         this.front =0;
+//     }
+//     enqueue(element){
+//         this.items[this.rear] = element
+//         this.rear++
+//     }
+//     dequeue(){
+//         const item = this.items[this.front]
+//         delete this.items[this.front]
+//         this.front++
+//         return item
+//     }
+//     peek(){
+//         return this.items[this.front]
+//     }
+//     isempty(){
+//         return this.rear - this.front ===0
+//     }
+//     size(){
+//         return this.rear -this.front
+//     }
+//     secondLargest(){
+//         let largest = this.items[0]
+//         let secLargest = this.items[1]
+//         if(largest<secLargest){
+//             [largest,secLargest] = [secLargest,largest]
+//         }
+//         for(let i=2;i<this.items.length;i++){
+//             if(this.items[i]>largest){
+//                 secLargest = largest
+//                 largest = secLargest
+//             }else if(this.items[i] > secLargest && this.items[i] !== largest){
+//                 secLargest = this.items[i]
+//             }
+//         }
+//         return [secLargest,largest]
+//     }
+//     print(){
+//         console.log(this.items);
+//     }
+// }
+
+// const q = new queue()
+// q.enqueue(10)
+// q.enqueue(200)
+// q.enqueue(30)
+// q.enqueue(20)
+
+// q.enqueue(10)
+
+// q.print()
+// console.log(q.secondLargest());
+
+// class queueUsingStack{
+//     constructor(){
+//         this.stack = new stack()
+//     }
+//     enqueue(data){
+//         this.stack.push(data)
+//     }
+//     dequeue(){
+//         this.stack.pop()
+//     }
+//     peek(){
+//        return this.stack.peek()
+//     }
+//     print(){
+//         this.stack.print()
+//     }
+// }
+
+// const queue = new queueUsingStack()
+// queue.enqueue(10)
+// queue.enqueue(20)
+// queue.enqueue(30)
+// queue.enqueue(40)
+// queue.print()
+// console.log(queue.peek());
 
 // class stack{
 //     constructor(){
-//         this.element = []
+//         this.element= []
 //     }
 //     push(data){
 //         this.element.push(data)
@@ -175,104 +284,111 @@
 //     peek(){
 //         return this.element[this.element.length-1]
 //     }
-//     min(){
-//         let min = this.element[0]
-//         let secondSmallest = this.element[1]
-//         if(min>secondSmallest){
-//             [min,secondSmallest] = [secondSmallest,min]
+//     largest(){
+//         let largest = this.element[0]
+//         for(let i=1;i<this.element.length;i++){
+//             if(this.element[i]>largest){
+//                 largest = this.element[i]
+//             }
+//         }
+//         return largest
+//     }
+//     smallest(){
+//         let smallest = this.element[0]
+//         for(let i=1;i<this.element.length;i++){
+//             if(this.element[i]<smallest){
+//                 smallest = this.element[i]
+//             }
+//         }
+//         return smallest
+//     }
+//     secondlargest(){
+//         let largest = this.element[0]
+//         let secondlargest = this.element[1]
+//         if(secondlargest>largest){
+//             [largest,secondlargest] = [secondlargest,largest]
 //         }
 //         for(let i=2;i<this.element.length;i++){
-//             if(this.element[i]<min){
-//                 secondSmallest = min
-//                 min  = this.element[i]
-//             }else if (this.element[i] < secondSmallest && this.element[i] !== min){
-//                 secondSmallest = this.element[i]
+//             if(this.element[i]>largest){
+//                 secondlargest = largest
+//                 largest = this.element[i]
+//             }else if(this.element[i]>secondlargest && this.element[i]!== largest){
+//                 secondlargest = this.element[i]
 //             }
 //         }
-//         return [min,secondSmallest]
+//         return [largest,secondlargest]
 //     }
-//     removeDups(){
-//         for(let i=0;i<this.element.length;i++){
-//             for(let j= i+1;j<this.element.length;j++){
-//                 if(this.element[i] === this.element[j]){
-//                     this.element.splice(j,1)
-//                     j--
+//     isValidParentheses(s) {
+//         const stack = [];
+    
+//         for (let char of s) {
+//             if (char === '(' || char === '[' || char === '{') {
+//                 stack.push(char);
+//             } else {
+//                 const lastOpenParenthesis = stack.pop();
+//                 if ((char === ')' && lastOpenParenthesis !== '(') ||(char === ']' && lastOpenParenthesis !== '[') ||(char === '}' && lastOpenParenthesis !== '{')) {
+//                     return false;
 //                 }
 //             }
 //         }
+//         return stack.length === 0;
 //     }
-//     validParanthisis(str){
-//         let stack =[]
-//         for(let char of str){
-//             if(char === "{" || char === "[" ||  char === "("){
-//                 stack.push(char)
-//             }else{
-//                 let oposite = stack.pop()
-//                 if((char === ")" && oposite !== "(") || (char ==="]" && oposite !== "[") || (char === "}" && oposite !== "{")){
-//                     return false
-//                 }
+//     secondSmallest(){
+//         let smallest = this.element[0]
+//         let secondSmallest = this.element[1]
+//         if(smallest>secondSmallest){
+//             [smallest,secondSmallest] = [secondSmallest,smallest]
+//         }
+//         for(let i=2;i<this.element.length;i++){
+//             if(this.element[i]<smallest){
+//                 secondSmallest = smallest
+//                 smallest = this.element[i]
+//             }if(this.element[i]<secondSmallest&& this.element[i]!==smallest){
+//                 secondSmallest = smallest
 //             }
 //         }
-//         return stack.length === 0
-//     }
-//     longsub(str){
-//         let longestsubstr =''
-//         for(let i=0;i<str.length;i++){
-//             for(let j=i+1;j<=str.length;j++){
-//                 let substr = str.substring(i,j)
-//                 if(str.indexOf(substr,j) !== -1 && substr.length> longestsubstr.length){
-//                     longestsubstr = substr
-//                 }
-//             }
-//         }
-//         return longestsubstr
+//         return [smallest,secondSmallest]
 //     }
 //     print(){
 //         console.log(this.element);
 //     }
 // }
+// const s = new stack()
+// s.push(20)
+// s.push(40)
+// s.push(10)
+// s.push(50)
+// s.push(60)
+// s.push(2)
+// s.print()
+// console.log(s.isValidParentheses("()"));
 
-// const st = new stack()
-// st.push(9)
-// st.push(2)
-// st.push(3)
-// st.push(7)
-// st.push(3)
-// st.print()
-// console.log(st.longsub("eeeeetttttvvvvvjjjjnnnnnnnnnn"));
-// st.print()
+// class StackUsingQueue {
+//     constructor() {
+//         this.queue = new queue();
+//     }
 
-// class queue{
-//     constructor(){
-//         this.element = []
-//         this.rear = 0
-//         this.front = 0
+//     push(element) {
+//         this.queue.enqueue(element);
 //     }
-//     enqueu(data){
-//         this.element[this.rear] = data
-//         this.rear++
+
+//     pop() {
+//         return this.queue.dequeue();
 //     }
-//     dequeue(){
-//         let item = this.element[this.front]
-//         delete this.element[this.front]
-//         this.front++
-//         return item
+
+//     peek() {
+
+//         return this.queue.peek();
 //     }
-//     peek(){
-//         return this.element[this.front]
-//     }
-//     print(){
-//         console.log(this.element);
+
+//     print() {
+//         // Print the elements in queue
+//         this.queue.print();
 //     }
 // }
-// const q = new queue()
-// q.enqueu(10)
-// q.enqueu(20)
-// q.enqueu(30)
-// q.enqueu(40)
-// q.enqueu(50)
-// q.print()
-// console.log(q.dequeue());
-// console.log(q.peek());
+// const stackq = new StackUsingQueue()
+// stackq.push(10)
+// stackq.push(20)
 
-
+// stackq.print()
+// console.log(stackq.peek());
