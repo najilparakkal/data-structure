68554c8db63ffce8eca442ccc6a21a1ed0910d0c
Najil Parakkal <najilparakkal@gmail.com>
2024-02-12 12:21:35 +0530

    week3

---
+++ trie.js
@@ -0,0 +1,54 @@
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+    insert(word) {
+        let currentNode = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i];
+            if (!currentNode.children[char]) {
+                currentNode.children[char] = new TrieNode();
+            }
+            currentNode = currentNode.children[char];
+        }
+        currentNode.isEndOfWord = true;
+    }
+
+    search(word) {
+        let currentNode = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i];
+            if (!currentNode.children[char]) {
+                return false;
+            }
+            currentNode = currentNode.children[char];
+        }
+        return currentNode.isEndOfWord;
+    }
+
+    startsWith(prefix) {
+        let currentNode = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            let char = prefix[i];
+            if (!currentNode.children[char]) {
+                return false;
+            }
+            currentNode = currentNode.children[char];
+        }
+        return true;
+    }
+
+}
+const trie = new Trie();
+trie.insert("app");
+trie.insert("apple");
+trie.insert("apply");
+
+console.log(trie.startsWith("app"));
+console.log(trie.search("ly"));
--- week 3.js
+++ week 3.js
@@ -2,7 +2,7 @@
 //     constructor(data) {
 //         this.data = data
 //         this.left = null
-//         this.rigth = null
+//         this.right = null
 //     }
 // }
 
@@ -14,60 +14,25 @@
 //         return this.root === null
 //     }
 //     insert(data) {
-//         const newNode = new node(data)
+//         let newNode = new node(data)
 //         if (this.isEmpty()) {
 //             this.root = newNode
 //         } else {
-//             this.insertNode(this.root, newNode)
+//             this.newNode(this.root, newNode)
 //         }
 //     }
-//     insertNode(root, newNode) {
-//         if (root.data > newNode.data) {
+//     newNode(root, node) {
+//         if (root.data > node.data) {
 //             if (root.left === null) {
-//                 root.left = newNode
+//                 root.left = node
 //             } else {
-//                 this.insertNode(root.left, newNode)
+//                 this.newNode(root.left, node)
 //             }
 //         } else {
-//             if (root.rigth === null) {
-//                 root.rigth = newNode
+//             if (root.right === null) {
+//                 root.right = node
 //             } else {
-//                 this.insertNode(root.rigth, newNode)
-//             }
-//         }
-//     }
-//     preOrder(root) {
-//         if (root) {
-//             console.log(root.data);
-//             this.preOrder(root.left)
-//             this.preOrder(root.rigth)
-//         }
-//     }
-//     inOrder(root) {
-//         if (root) {
-//             this.inOrder(root.left)
-//             console.log(root.data);
-//             this.inOrder(root.rigth)
-//         }
-//     }
-//     postOrder(root) {
-//         if (root) {
-//             this.postOrder(root.left)
-//             this.postOrder(root.rigth)
-//             console.log(root.data);
-//         }
-//     }
-//     levelOrder() {
-//         let queue = []
-//         queue.push(this.root)
-//         while (queue.length) {
-//             let curr = queue.shift()
-//             console.log(curr.data);
-//             if (curr.left) {
-//                 queue.push(curr.left)
-//             }
-//             if (curr.rigth) {
-//                 queue.push(curr.rigth)
+//                 this.newNode(root.right, node)
 //             }
 //         }
 //     }
@@ -80,7 +45,7 @@
 //             } else if (root.data > data) {
 //                 return this.searchData(root.left, data)
 //             } else {
-//                 return this.searchData(root.rigth, data)
+//                 return this.searchData(root.right, data)
 //             }
 //         }
 //     }
@@ -91,55 +56,86 @@
 //             return this.min(root.left)
 //         }
 //     }
-//     max(root) {
-//         if (!root.rigth) {
-//             return root.data
-//         } else {
-//             return this.max(root.rigth)
-//         }
-//     }
 //     remove(data) {
-//         this.removeData(this.root, data)
+//         this.removeNode(this.root,data)
 //     }
-//     removeData(root, data) {
+//     removeNode(root, data) {
 //         if (root === null) {
 //             return root
 //         }
-//         if (root.data > data) {
-//             root.left = this.removeData(root.left, data)
-//         } else if (root.data < data) {
-//             root.rigth = this.removeData(root.rigth, data)
+//         if (data < root.data) {
+//             root.left = this.removeNode(root.left, data)
+//         } else if (data > root.data) {
+//             root.right = this.removeNode(root.right,data)
 //         } else {
-//             if (!root.left && !root.rigth) {
+//             if (!root.left && !root.right) {
 //                 return null
 //             }
 //             if (!root.left) {
-//                 return root.rigth
-//             } else if (!root.rigth) {
+//                 return root.right
+//             } else if (!root.right) {
 //                 return root.left
 //             }
-//             root.data = this.min(root.rigth)
-//             root.rigth = this.removeData(root.rigth, root.data)
+//             root.data = this.min(root.right)
+//             root.right = this.removeNode(root.right, root.data)
 //         }
 //         return root
 //     }
-
+//     levelOrder() {
+//         let queue = []
+//         queue.push(this.root)
+//         while (queue.length) {
+//             let curr = queue.shift()
+//             console.log(curr.data);
+//             if (curr.left) {
+//                 queue.push(curr.left)
+//             }
+//             if (curr.right) {
+//                 queue.push(curr.right)
+//             }
+//         }
+//     }
+//     preOrder(root) {
+//         if (root) {
+//             console.log(root.data);
+//             this.preOrder(root.left)
+//             this.preOrder(root.right)
+//         }
+//     }
+//     inOrder(root) {
+//         if (root) {
+//             this.inOrder(root.left)
+//             console.log(root.data);
+//             this.inOrder(root.right)
+//         }
+//     }
+//     postOrder(root) {
+//         if (root) {
+//             this.postOrder(root.left)
+//             this.postOrder(root.right)
+//             console.log(root.data);
+//         }
+//     }
 // }
-// const BST = new BinerySearchTree()
-// BST.insert(10)
-// BST.insert(20)
-// BST.insert(5)
-// BST.insert(8)
-// BST.insert(3)
-// BST.inOrder(BST.root)
+// const bst = new BinerySearchTree()
+// bst.insert(10)
+// bst.insert(20)
+// bst.insert(5)
+// bst.insert(8)
+// bst.insert(3)
+// bst.levelOrder()
+// console.log(bst.searchData(bst.root, 10));
+// console.log(bst.searchData(bst.root, 20));
+// console.log(bst.searchData(bst.root, 5));
+// console.log(bst.searchData(bst.root, 8));
+// console.log(bst.searchData(bst.root, 3));
+// console.log(bst.min(bst.root));
+// bst.remove(5)
+// bst.levelOrder()
 
-// console.log(BST.searchData(BST.root, 100));
-// BST.remove(5)
-// BST.inOrder(BST.root)
 
 
-
-// class AdjecencyList {
+// class Graph {
 //     constructor() {
 //         this.list = {}
 //     }
@@ -163,107 +159,113 @@
 //             this.list[vertex1].has(vertex2) && this.list[vertex2].has(vertex1)
 //         )
 //     }
-//     removeVertex(vertex) {
-//         if (!this.list[vertex]) {
-//             return console.log("Already Deleted");
-//         }
-//         for (let vert of this.list[vertex]) {
-//             this.removeEdge(vertex, vert)
-//         }
-//         delete this.list[vertex]
-//     }
 //     removeEdge(vertex1, vertex2) {
 //         this.list[vertex1].delete(vertex2)
 //         this.list[vertex2].delete(vertex1)
 //     }
+//     removeVertex(vertex) {
+//         if (!this.list[vertex]) {
+//             return undefined
+//         }
+//         for (let vert of this.list[vertex]) {
+//             this.removeEdge(vert, vertex)
+//         }
+//         delete this.list[vertex]
+//     }
 //     display() {
 //         for (let vertex in this.list) {
-//             console.log(vertex + "=>" + [...this.list[vertex]]);
+//             console.log(vertex + " => " + [...this.list[vertex]]);
 //         }
 //     }
 // }
-// const adjList = new AdjecencyList()
-// adjList.addVertex("A")
-// adjList.addVertex("B")
-// adjList.addVertex("C")
-// adjList.addEdges("A", "B")
-// adjList.addEdges("B", "C")
-// adjList.display()
-// adjList.removeVertex("A")
-// adjList.display()
+// const graph = new Graph()
+// graph.addVertex("A")
+// graph.addVertex("B")
+// graph.addVertex("C")
+// graph.addEdges("A", "B")
+// graph.addEdges("B", "C")
+// graph.display()
+// console.log(graph.hasEdge("A", "B"));
+// graph.removeVertex("B")
+// graph.display()
 
 
 
-class MinHeap {
-    constructor() {
-        this.heap = []
-    }
-    getParent(index) {
-        return Math.floor((index - 1) / 2)
-    }
-    getLeft(index) {
-        return 2 * index + 1
-    }
-    getRight(index) {
-        return 2 * index + 2
-    }
-    swap(index1, index2) {
-        let temp = this.heap[index1]
-        this.heap[index1] = this.heap[index2]
-        this.heap[index2] = temp
-    }
-    insert(data) {
-        this.heap.push(data)
-        this.heapyfiUp(this.heap.length - 1)
-    }
-    heapyfiUp(index) {
-        let parent = this.getParent(index)
-        if (parent >= 0 && this.heap[parent] > this.heap[index]) {
-            this.swap(parent, index)
-            this.heapyfiUp(parent)
-        }
-    }
-    remove(){
-        if(this.heap.length === 0){
-            return null
-        }
-        if(this.heap.length === 1){
-           return this.heap.pop()
-        }
-        let min = this.heap[0]
-        this.heap[0] = this.heap.pop()
-        this.heapyfiDown(0)
-        return min
-    }
-    heapyfiDown(index){
-        let left = this.getLeft(index)
-        let right = this.getRight(index)
-        let min = index
-        if(left<this.heap.length && this.heap[left]<this.heap[min]){
-            min = left
-        }
-        if(right<this.heap.length && this.heap[right]<this.heap[min]){
-            min = right
-        }
-        if(min!== index){
-            this.swap(min,index)
-            this.heapyfiDown(min)
-        }
-    }
-    heapSort(){
-        let arr = []
-        while(this.heap.length){
-            arr.push(this.remove())
-        }
-        return arr
-    }
-    
-}
-const heap = new MinHeap()
-heap.insert(10)
-heap.insert(38)
-heap.insert(2)
-heap.insert(46)
-heap.insert(34)
-console.log(heap.heap);
-console.log(heap.heapSort());
+
+
+// class MinHeap {
+//     constructor() {
+//         this.heap = []
+//     }
+//     getParent(index) {
+//         return Math.floor((index - 1) / 2)
+//     }
+//     getLeft(index){
+//         return 2*index+1
+//     }
+//     getRight(index){
+//         return 2*index +2
+//     }
+//     swap(index1, index2) {
+//         let temp = this.heap[index1]
+//         this.heap[index1] = this.heap[index2]
+//         this.heap[index2] = temp
+//     }
+//     insert(data) {
+//         this.heap.push(data)
+//         this.heapifyUp(this.heap.length - 1)
+//     }
+//     heapifyUp(index) {
+//         let parent = this.getParent(index)
+//         if (parent >= 0 && this.heap[parent] > this.heap[index]) {
+//             this.swap(index, parent)
+//             this.heapifyUp(parent)
+//         }
+//     }
+//     remove() {
+//         if (this.heap.length === 0) {
+//             return null
+//         }
+//         if(this.heap.length === 1){
+//             return this.heap.pop()
+//         }
+//         let min = this.heap[0]
+//         this.heap[0] = this.heap.pop()
+//         this.heapifyDown(0)
+//         return min
+//     }
+//     heapifyDown(index){
+//         let left = this.getLeft(index)
+//         let right = this.getRight(index)
+//         let min = index
+//         if(left<this.heap.length && this.heap[left]<this.heap[min]){
+//             min = left
+//         }
+//         if(right<this.heap.length && this.heap[right]<this.heap[min]){
+//             min = right
+//         }
+//         if(min != index){
+//             this.swap(min,index)
+//             this.heapifyDown(min)
+//         }
+//     }
+//     heapSort(){
+//         let arr = []
+//         while(this.heap.length){
+//             arr.push(this.remove())
+//         }
+//         return arr
+//     }
+
+// }
+// const minHeap = new MinHeap()
+// minHeap.insert(10)
+// minHeap.insert(5)
+// minHeap.insert(8)
+// minHeap.insert(20)
+// minHeap.insert(3)
+// console.log(minHeap.heap);
+// minHeap.remove()
+// console.log(minHeap.heap);
+// console.log(minHeap.heapSort());
+
