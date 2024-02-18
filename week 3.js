4055add5e03be3ea8357484abdddbb842c4df900
Najil Parakkal <najilparakkal@gmail.com>
2024-02-18 13:59:33 +0530

    week3

--- week 3.js
+++ week 3.js
@@ -5,7 +5,7 @@
 //         this.right = null
 //     }
 // }
-// class BST {
+// class Bst {
 //     constructor() {
 //         this.root = null
 //     }
@@ -35,98 +35,128 @@
 //             }
 //         }
 //     }
-//     height(root) {
-//         if (!root) {
-//             return -1
-//         } else {
-//             let left = this.height(root.left)
-//             let right = this.height(root.right)
-//             return Math.max(left, right) + 1
-//         }
-//     }
 //     searchData(root, data) {
-//         if(!root){
-//             return 
+//         if (!root) {
+//             return null
 //         }
-//         if(root.data === data){
+//         if (root.data === data) {
 //             return true
-//         }else if(root.data>data){
-//             return this.searchData(root.left,data)
-//         }else{
-//             return this.searchData(root.right,data)
+//         } else if (root.data > data) {
+//             return this.searchData(root.left, data)
+//         } else {
+//             return this.searchData(root.right, data)
 //         }
 //     }
-//     min(root){
-//         if(!root.left){
+//     validate() {
+//         return this.isValid(this.root, -Infinity, Infinity)
+//     }
+//     isValid(root, min, max) {
+//         if (!root) {
+//             return true
+//         }
+//         if (root.data < min || root.data > max) {
+//             return false
+//         }
+//         return this.isValid(root.left, min, max) && this.isValid(root.right, root.data, max)
+//     }
+//     min(root) {
+//         if (!root.left) {
 //             return root.data
-//         }else{
+//         } else {
 //             this.min(root.left)
 //         }
 //     }
-//     remove(data){
-//         this.removeNode(this.root,data)
-//     }
-//     removeNode(root,data){
-//         if(!root){
-//             return root
+//     max(root) {
+//         if (!root.right) {
+//             return root.data
+//         } else {
+//             this.max(root.right)
 //         }
-//         if(root.data>data){
-//             root.left = this.removeNode(root.left,data)
-//         }else if (root.data < data){
-//             root.right = this.removeNode(root.right,data)
-//         }else{
-//             if(!root.right&& !root.left){
+//     }
+//     height(root) {
+//         if (root === null) {
+//             return null
+//         } else {
+//             const left = this.height(root.left)
+//             const right = this.height(root.right)
+//             return Math.max(left, right) + 1
+//         }
+
+//     }
+//     remove(data) {
+//         this.removeData(this.root, data)
+//     }
+//     removeData(root, data) {
+//         if (!root) {
+//             return null
+//         }
+//         if (root.data > data) {
+//             root.left = this.removeData(root.left, data)
+//         } else if (root.data < data) {
+//             root.right = this.removeData(root.right, data)
+//         } else {
+//             if (!root.right && !root.left) {
 //                 return null
 //             }
-//             if(!root.left){
+//             if (!root.left) {
 //                 return root.right
-//             }else if(!root.right){
+//             } else if (!root.right) {
 //                 return root.left
 //             }
 //             root.data = this.min(root.right)
-//             root.right = this.removeNode(root.right,root.data)
+//             root.right = this.removeData(root.right, root.data)
 //         }
 //         return root
 //     }
-//     validat(){
-//         return this.isValid(this.root,-Infinity,Infinity)
-//     }
-//     isValid(root,min,max){
-//         if(!root){
-//             return true
-//         }
-//         if(root.data>max || root.data< min){
-//             return false
-//         }
-//         return this.isValid(root.left,min ,root.data)  && this.isValid(root.right,root.data,max)
-//     }
-//     levelOrder(){
+//     levelOrder() {
 //         let queue = []
 //         queue.push(this.root)
-//         while(queue.length){
+//         while (queue.length) {
 //             let curr = queue.shift()
 //             console.log(curr.data);
-//             if(curr.left){
+//             if (curr.left) {
 //                 queue.push(curr.left)
 //             }
-//             if(curr.right){
+//             if (curr.right) {
 //                 queue.push(curr.right)
 //             }
 //         }
 //     }
+//     preOrder(root) {
+//         if (root) {
+//             console.log(root.data);
+//             this.preOrder(root.left)
+//             this.preOrder(root.right)
+//         }
+//     }
+//     inOrder(root) {
+//         if (root) {
+//             this.inOrder(root.left)
+//             console.log(root.data);
+//             this.inOrder(root.right)
+//         }
+//     }
+//     postOrder(root) {
+//         if (root) {
+//             this.postOrder(root.left)
+//             this.postOrder(root.right)
+//             console.log(root.data);
+//         }
+//     }
 // }
-// const tree = new BST()
+// const tree = new Bst()
 // tree.insert(10)
-// tree.insert(5)
 // tree.insert(20)
-// tree.insert(8)
+// tree.insert(5)
 // tree.insert(3)
+// tree.insert(8)
 // tree.remove(5)
+// console.log(tree.validate());
+// console.log(tree.height(tree.root));
 // tree.levelOrder()
-// console.log(tree.isValid());
 
 
-// class graph {
+// class Graph{
 //     constructor(){
 //         this.list = {}
 //     }
@@ -145,233 +175,104 @@
 //         this.list[vertex1].add(vertex2)
 //         this.list[vertex2].add(vertex1)
 //     }
-//     removeEdges(vertex1,vertex2){
-//         this.list[vertex1].delete(vertex1)
-//         this.list[vertex2].delete(vertex2)
+//     hasEdge(vertex1,vertex2){
+//         return (this.list[vertex1].has(vertex2) && this.list[vertex2].has(vertex1))
+//     }
+//     removeEdge(vertex1,vertex2){
+//         this.list[vertex1].delete(vertex2)
+//         this.list[vertex2].delete(vertex1)
 //     }
 //     removeVertex(vertex){
 //         if(!this.list[vertex]){
 //             return null
 //         }
-//         for(let node of this.list[vertex]){
-//             this.removeEdges(node,vertex)
+//         for(let vert of this.list[vertex]){
+//             this.removeEdge(vert,vertex)
 //         }
 //         delete this.list[vertex]
 //     }
 //     bfs(vertex){
+//         let visited ={}
 //         let queue = [vertex]
-//         let visited = {}
 //         visited[vertex] = true
 //         while(queue.length){
 //             let curr = queue.shift()
 //             console.log(curr);
-//             for(let neighbor of this.list[curr]){
-//                 if(!visited[neighbor]){
-//                     visited[neighbor] = true
-//                     queue.push(neighbor)
+//             for(let sibling of this.list[curr]){
+//                 if(!visited[sibling]){
+//                     visited[sibling] = true
+//                     queue.push(sibling)
 //                 }
 //             }
 //         }
 //     }
-//     display(){
-//         for(let vert in this.list){
-//             console.log(vert + "=> "+ [...this.list[vert]]);
-//         }
-//     }
-// }
-
-// const gr = new graph()
-// gr.addVertex("A")
-// gr.addVertex("B")
-// gr.addVertex("C")
-// gr.addEdges("A","B")
-// gr.addEdges("C","B")
-// gr.display()
-// gr.bfs("A")
-
-// class trieNode{
-//     constructor(){
-//         this.child = {}
-//         this.end = false
-//     }
-// }
-// class Trie{
-//     constructor(){
-//         this.root = new trieNode()
-//     }
-//     insert(word){
-//         let curr = this.root
-//         for(let i = 0;i<word.length;i++){
-//             let char = word[i]
-//             if(!curr.child[char]){
-//                 curr.child[char] = new trieNode()
-//             }
-//             curr = curr.child[char]
-//         }
-//         curr.end = true
-//     }
-//     serch(word){
-//         let curr = this.root
-//         for(let i=0;i<word.length;i++){
-//             let char = word[i]
-//             if(!curr.child[char]){
-//                 return false
-//             }
-//             curr = curr.child[char]
-//         }
-//         return curr.end
-//     }
-//     prefix(word){
-//         let curr = this.root
-//         for(let i=0;i<word.length;i++){
-//             let char = word[i]
-//             if(!curr.child[char]){
-//                 return false
-//             }
-//             curr = curr.child[char]
-//         }
-//         return true
-//     }
-// }
-// const trie = new Trie()
-// trie.insert("HELLO")
-// console.log(trie.prefix("HEL"));
-// console.log(trie.serch("HEL"));
-
-
-// class Heap{
-//     constructor(){
-//         this.heap = []
-//     }
-//     getParent(index){
-//         return Math.floor((index-1)/2)
-//     }
-//     getLeft(index){
-//         return 2*index+1
-//     }
-//     getRight(index){
-//         return 2*index+2
-//     }
-//     swap(index1,index2){
-//         let temp = this.heap[index1]
-//         this.heap[index1] = this.heap[index2]
-//         this.heap[index2] = temp
-//     }
-//     insert(data){
-//         this.heap.push(data)
-//         this.heapifyUp(this.heap.length-1)
-//     }
-//     heapifyUp(index){
-//         let parent = this.getParent(index)
-//         if(parent<this.heap.length && this.heap[parent]>this.heap[index]){
-//             this.swap(parent,index)
-//             this.heapifyUp(parent)
-//         }
-//     }
-//     remove(){
-//         if(this.heap.length ===0){
-//             return null
-//         }
-//         if(this.heap.length === 1){
-//             return this.heap.pop()
-//         }
-//         let min = this.heap[0]
-//         this.heap[0] = this.heap.pop()
-//         this.heapifyDown(0)
-//         return min
-//     }
-//     heapifyDown(index){
-//         let left = this.getLeft(index)
-//         let right = this.getRight(index)
-//         let min = index
-//         if(left<this.heap.length && this.heap[left]<this.heap[min]){
-//             min = left
-//         }
-//         if(right<this.heap.length && this.heap[right]<this.heap[min]){
-//             min = right
-//         }
-//         if(min != index){
-//             this.swap(min,index)
-//             this.heapifyDown(min)
-//         }
-//     }
-// }
-// const heap = new Heap()
-// heap.insert(10)
-// heap.insert(15)
-// heap.insert(20)
-// heap.insert(2)
-// heap.remove()
-// console.log(heap.heap);
-
-
-//  class priorityQueue{
-//     constructor(){
-//         this.heap = []
-//     }
-//         getParent(index){
-//         return Math.floor((index-1)/2)
-//     }
-//     getLeft(index){
-//         return 2*index+1
-//     }
-//     getRight(index){
-//         return 2*index+2
-//     }
-//     swap(index1,index2){
-//         let temp = this.heap[index1]
-//         this.heap[index1] = this.heap[index2]
-//         this.heap[index2] = temp
-//     }
-//     insert(data,priority){
-//         let item = {data,priority}
-
-//         this.heap.push(item)
-//         this.heapifyUp(this.heap.length-1)
-//     }
-//     heapifyUp(index){
-//         let parent = this.getParent(index)
-//         while(index >0 && this.heap[parent].priority >this.heap[index].priority){
-//             this.swap(index,parent)
-//             index = parent
-//         }
-//     }
-//     remove(){
-//         if(this.heap.length ===0){
-//             return null
-//         }
-//         if(this.heap.length === 1){
-//             return this.heap.pop()
-//         }
-//         let min = this.heap[0]
-//         this.heap[0] = this.heap.pop()
-//         this.heapifyDown(0)
-//         return min
-//     }
-//     heapifyDown(index){
-//         let left = this.getLeft(index)
-//         let right = this.getRight(index)
-//         let min = index
-//         if(left<this.heap.length && this.heap[left].priority<this.heap[min].priority){
-//             min = left
-//         }
-//         if(right<this.heap.length && this.heap[right].priority<this.heap[min].priority){
-//             min = right
-//         }
-//         if(min != index){
-//             this.swap(min,index)
-//             this.heapifyDown(min)
-//         }
-//     }
 //     isEmpty(){
-//         return this.heap.length === 0  
+//         return this.list.length === 0
+//     }
+//     display(){
+//         for(let vertex in this.list){
+//             console.log(vertex +" = >"+[...this.list[vertex]]);
+//         }
 //     }
 // }
-// const queue = new priorityQueue()
-// queue.insert("Task 1", 3 )
-// queue.insert("Task 2", 1 )
-// queue.insert("Task 3", 2 )
-// while(!queue.isEmpty()){
-//     let item = queue.remove()
-//     console.log(`${item.data} => priority ${item.priority}`);
-// }
+// const graph = new Graph()
+// graph.addVertex("A")
+// graph.addVertex("B")
+// graph.addVertex("C")
+// graph.addEdges("A","B")
+// graph.addEdges("C","B")
+// graph.removeVertex("B")
+// graph.display()
+// graph.bfs("A")
+
+
+
+
+class node {
+    constructor() {
+        this.child = {}
+        this.end = false
+    }
+}
+class Trie {
+    constructor() {
+        this.root = new node()
+    }
+    insert(word) {
+        const currend = this.root
+        for (let i = 0; i < word.length; i++) {
+            let char = word[i]
+            if (!currend.child[char]) {
+                currend.child[char] = new node()
+            }
+            currend = currend.child[char]
+        }
+        currend.end = true
+    }
+    search(word) {
+        const currend = this.root
+        for (let i = 0; i < word.length; i++) {
+            let char = word[i]
+            if (!currend.child[char]) {
+                return false
+            }
+            currend = currend.child[char]
+        }
+        return currend.end
+    }
+    Prefix(word) {
+        const currend = this.root
+        for (let i = 0; i < word.length; i++) {
+            let char = word[i]
+            if (!currend.child[char]) {
+                return false
+            }
+            currend = currend.child[char]
+        }
+        return true
+    }
+}
+const trie = new Trie()
+trie.insert("NAJIL")
+trie.search("NA")
+trie.Prefix("NA")
